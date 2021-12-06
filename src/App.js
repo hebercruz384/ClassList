706d3d3c83a97a70aea1cfc4cae0437b5fd182ea
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2021-12-06 18:25:23 -0500

    Added Schools

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import "./styles.css";
 
 function ListItem(personData) {
   return (
-    <tr>
+    
+   <tr>  
       <td>{personData.firstname}</td>
       <td>{personData.lastname}</td>
       <td>{personData.pronouns}</td>
+      <td>{personData.school}</td>
     </tr>
+    
   );
 }
 let classRoster = [
-  { firstname: "Heber", lastname: "Cruz", pronouns: "he/him" },
-  { firstname: "Chinaza", lastname: "Onwukanjo", pronouns: "she/her" },
-  { firstname: "Alex", lastname: "Sanchez", pronouns: "he/him" },
-  { firstname: "Claudia", lastname: "Luna-Marcelino", pronouns: "she/her" }
+  { firstname : "First Name", lastname : "Last Name", pronouns : "Pronouns", school : "School"},
+  { firstname: "Heber", lastname: "Cruz", pronouns: "he/him" , school : "John Jay School for Law"},
+  { firstname: "Chinaza", lastname: "Onwukanjo", pronouns: "she/her" ,school : "Frederick Douglass Academy" },
+  { firstname: "Alex", lastname: "Sanchez", pronouns: "he/him" ,school : ""},
+  { firstname: "Claudia", lastname: "Luna-Marcelino", pronouns: "she/her", school : "P-TECH"}
 ];
 let rosterList = classRoster.map(ListItem);
 
