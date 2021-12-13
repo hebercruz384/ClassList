5c46951808ee35857a93840d636248b6a6237bb7
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2021-12-13 17:48:22 -0500

    made the zebra stripes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import "./styles.css";
 
 function ListItem(personData) {
   return (
-    
-   <tr>  
+    <tr>
       <td>{personData.firstname}</td>
       <td>{personData.lastname}</td>
       <td>{personData.pronouns}</td>
       <td>{personData.school}</td>
     </tr>
-    
   );
 }
 let classRoster = [
-  { firstname : "First Name", lastname : "Last Name", pronouns : "Pronouns", school : "School"},
-  { firstname: "Heber", lastname: "Cruz", pronouns: "he/him" , school : "John Jay School for Law"},
-  { firstname: "Chinaza", lastname: "Onwukanjo", pronouns: "she/her" ,school : "Frederick Douglass Academy" },
-  { firstname: "Alex", lastname: "Sanchez", pronouns: "he/him" ,school : ""},
-  { firstname: "Claudia", lastname: "Luna-Marcelino", pronouns: "she/her", school : "P-TECH"}
+  {
+    firstname: "First Name",
+    lastname: "Last Name",
+    pronouns: "Pronouns",
+    school: "School"
+  },
+  {
+    firstname: "Heber",
+    lastname: "Cruz",
+    pronouns: "he/him",
+    school: "John Jay School for Law"
+  },
+  {
+    firstname: "Chinaza",
+    lastname: "Onwukanjo",
+    pronouns: "she/her",
+    school: "Frederick Douglass Academy"
+  },
+  { firstname: "Alex", lastname: "Sanchez", pronouns: "he/him", school: "" },
+  {
+    firstname: "Claudia",
+    lastname: "Luna-Marcelino",
+    pronouns: "she/her",
+    school: "P-TECH"
+  }
 ];
 let rosterList = classRoster.map(ListItem);
 
